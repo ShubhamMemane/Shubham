301712e667291a30efd21768cfe80c67ca6b4e04
ShubhamMemane <43327524+ShubhamMemane@users.noreply.github.com>
2020-01-13 16:29:09 +0530

    Add files via upload

---
+++ books.java
@@ -0,0 +1,519 @@
+// ORM class for table 'books'
+// WARNING: This class is AUTO-GENERATED. Modify at your own risk.
+//
+// Debug information:
+// Generated date: Fri Nov 15 13:02:43 IST 2019
+// For connector: org.apache.sqoop.manager.MySQLManager
+import org.apache.hadoop.io.BytesWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.mapred.lib.db.DBWritable;
+import com.cloudera.sqoop.lib.JdbcWritableBridge;
+import com.cloudera.sqoop.lib.DelimiterSet;
+import com.cloudera.sqoop.lib.FieldFormatter;
+import com.cloudera.sqoop.lib.RecordParser;
+import com.cloudera.sqoop.lib.BooleanParser;
+import com.cloudera.sqoop.lib.BlobRef;
+import com.cloudera.sqoop.lib.ClobRef;
+import com.cloudera.sqoop.lib.LargeObjectLoader;
+import com.cloudera.sqoop.lib.SqoopRecord;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.sql.Date;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
+public class books extends SqoopRecord  implements DBWritable, Writable {
+  private final int PROTOCOL_VERSION = 3;
+  public int getClassFormatVersion() { return PROTOCOL_VERSION; }
+  public static interface FieldSetterCommand {    void setField(Object value);  }  protected ResultSet __cur_result_set;
+  private Map<String, FieldSetterCommand> setters = new HashMap<String, FieldSetterCommand>();
+  private void init0() {
+    setters.put("id", new FieldSetterCommand() {
+      @Override
+      public void setField(Object value) {
+        books.this.id = (Integer)value;
+      }
+    });
+    setters.put("name", new FieldSetterCommand() {
+      @Override
+      public void setField(Object value) {
+        books.this.name = (String)value;
+      }
+    });
+    setters.put("author", new FieldSetterCommand() {
+      @Override
+      public void setField(Object value) {
+        books.this.author = (String)value;
+      }
+    });
+    setters.put("subject", new FieldSetterCommand() {
+      @Override
+      public void setField(Object value) {
+        books.this.subject = (String)value;
+      }
+    });
+    setters.put("price", new FieldSetterCommand() {
+      @Override
+      public void setField(Object value) {
+        books.this.price = (Float)value;
+      }
+    });
+  }
+  public books() {
+    init0();
+  }
+  private Integer id;
+  public Integer get_id() {
+    return id;
+  }
+  public void set_id(Integer id) {
+    this.id = id;
+  }
+  public books with_id(Integer id) {
+    this.id = id;
+    return this;
+  }
+  private String name;
+  public String get_name() {
+    return name;
+  }
+  public void set_name(String name) {
+    this.name = name;
+  }
+  public books with_name(String name) {
+    this.name = name;
+    return this;
+  }
+  private String author;
+  public String get_author() {
+    return author;
+  }
+  public void set_author(String author) {
+    this.author = author;
+  }
+  public books with_author(String author) {
+    this.author = author;
+    return this;
+  }
+  private String subject;
+  public String get_subject() {
+    return subject;
+  }
+  public void set_subject(String subject) {
+    this.subject = subject;
+  }
+  public books with_subject(String subject) {
+    this.subject = subject;
+    return this;
+  }
+  private Float price;
+  public Float get_price() {
+    return price;
+  }
+  public void set_price(Float price) {
+    this.price = price;
+  }
+  public books with_price(Float price) {
+    this.price = price;
+    return this;
+  }
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof books)) {
+      return false;
+    }
+    books that = (books) o;
+    boolean equal = true;
+    equal = equal && (this.id == null ? that.id == null : this.id.equals(that.id));
+    equal = equal && (this.name == null ? that.name == null : this.name.equals(that.name));
+    equal = equal && (this.author == null ? that.author == null : this.author.equals(that.author));
+    equal = equal && (this.subject == null ? that.subject == null : this.subject.equals(that.subject));
+    equal = equal && (this.price == null ? that.price == null : this.price.equals(that.price));
+    return equal;
+  }
+  public boolean equals0(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof books)) {
+      return false;
+    }
+    books that = (books) o;
+    boolean equal = true;
+    equal = equal && (this.id == null ? that.id == null : this.id.equals(that.id));
+    equal = equal && (this.name == null ? that.name == null : this.name.equals(that.name));
+    equal = equal && (this.author == null ? that.author == null : this.author.equals(that.author));
+    equal = equal && (this.subject == null ? that.subject == null : this.subject.equals(that.subject));
+    equal = equal && (this.price == null ? that.price == null : this.price.equals(that.price));
+    return equal;
+  }
+  public void readFields(ResultSet __dbResults) throws SQLException {
+    this.__cur_result_set = __dbResults;
+    this.id = JdbcWritableBridge.readInteger(1, __dbResults);
+    this.name = JdbcWritableBridge.readString(2, __dbResults);
+    this.author = JdbcWritableBridge.readString(3, __dbResults);
+    this.subject = JdbcWritableBridge.readString(4, __dbResults);
+    this.price = JdbcWritableBridge.readFloat(5, __dbResults);
+  }
+  public void readFields0(ResultSet __dbResults) throws SQLException {
+    this.id = JdbcWritableBridge.readInteger(1, __dbResults);
+    this.name = JdbcWritableBridge.readString(2, __dbResults);
+    this.author = JdbcWritableBridge.readString(3, __dbResults);
+    this.subject = JdbcWritableBridge.readString(4, __dbResults);
+    this.price = JdbcWritableBridge.readFloat(5, __dbResults);
+  }
+  public void loadLargeObjects(LargeObjectLoader __loader)
+      throws SQLException, IOException, InterruptedException {
+  }
+  public void loadLargeObjects0(LargeObjectLoader __loader)
+      throws SQLException, IOException, InterruptedException {
+  }
+  public void write(PreparedStatement __dbStmt) throws SQLException {
+    write(__dbStmt, 0);
+  }
+
+  public int write(PreparedStatement __dbStmt, int __off) throws SQLException {
+    JdbcWritableBridge.writeInteger(id, 1 + __off, 4, __dbStmt);
+    JdbcWritableBridge.writeString(name, 2 + __off, 12, __dbStmt);
+    JdbcWritableBridge.writeString(author, 3 + __off, 12, __dbStmt);
+    JdbcWritableBridge.writeString(subject, 4 + __off, 12, __dbStmt);
+    JdbcWritableBridge.writeFloat(price, 5 + __off, 7, __dbStmt);
+    return 5;
+  }
+  public void write0(PreparedStatement __dbStmt, int __off) throws SQLException {
+    JdbcWritableBridge.writeInteger(id, 1 + __off, 4, __dbStmt);
+    JdbcWritableBridge.writeString(name, 2 + __off, 12, __dbStmt);
+    JdbcWritableBridge.writeString(author, 3 + __off, 12, __dbStmt);
+    JdbcWritableBridge.writeString(subject, 4 + __off, 12, __dbStmt);
+    JdbcWritableBridge.writeFloat(price, 5 + __off, 7, __dbStmt);
+  }
+  public void readFields(DataInput __dataIn) throws IOException {
+this.readFields0(__dataIn);  }
+  public void readFields0(DataInput __dataIn) throws IOException {
+    if (__dataIn.readBoolean()) { 
+        this.id = null;
+    } else {
+    this.id = Integer.valueOf(__dataIn.readInt());
+    }
+    if (__dataIn.readBoolean()) { 
+        this.name = null;
+    } else {
+    this.name = Text.readString(__dataIn);
+    }
+    if (__dataIn.readBoolean()) { 
+        this.author = null;
+    } else {
+    this.author = Text.readString(__dataIn);
+    }
+    if (__dataIn.readBoolean()) { 
+        this.subject = null;
+    } else {
+    this.subject = Text.readString(__dataIn);
+    }
+    if (__dataIn.readBoolean()) { 
+        this.price = null;
+    } else {
+    this.price = Float.valueOf(__dataIn.readFloat());
+    }
+  }
+  public void write(DataOutput __dataOut) throws IOException {
+    if (null == this.id) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    __dataOut.writeInt(this.id);
+    }
+    if (null == this.name) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    Text.writeString(__dataOut, name);
+    }
+    if (null == this.author) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    Text.writeString(__dataOut, author);
+    }
+    if (null == this.subject) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    Text.writeString(__dataOut, subject);
+    }
+    if (null == this.price) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    __dataOut.writeFloat(this.price);
+    }
+  }
+  public void write0(DataOutput __dataOut) throws IOException {
+    if (null == this.id) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    __dataOut.writeInt(this.id);
+    }
+    if (null == this.name) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    Text.writeString(__dataOut, name);
+    }
+    if (null == this.author) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    Text.writeString(__dataOut, author);
+    }
+    if (null == this.subject) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    Text.writeString(__dataOut, subject);
+    }
+    if (null == this.price) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    __dataOut.writeFloat(this.price);
+    }
+  }
+  private static final DelimiterSet __outputDelimiters = new DelimiterSet((char) 44, (char) 10, (char) 0, (char) 0, false);
+  public String toString() {
+    return toString(__outputDelimiters, true);
+  }
+  public String toString(DelimiterSet delimiters) {
+    return toString(delimiters, true);
+  }
+  public String toString(boolean useRecordDelim) {
+    return toString(__outputDelimiters, useRecordDelim);
+  }
+  public String toString(DelimiterSet delimiters, boolean useRecordDelim) {
+    StringBuilder __sb = new StringBuilder();
+    char fieldDelim = delimiters.getFieldsTerminatedBy();
+    __sb.append(FieldFormatter.escapeAndEnclose(id==null?"null":"" + id, delimiters));
+    __sb.append(fieldDelim);
+    __sb.append(FieldFormatter.escapeAndEnclose(name==null?"null":name, delimiters));
+    __sb.append(fieldDelim);
+    __sb.append(FieldFormatter.escapeAndEnclose(author==null?"null":author, delimiters));
+    __sb.append(fieldDelim);
+    __sb.append(FieldFormatter.escapeAndEnclose(subject==null?"null":subject, delimiters));
+    __sb.append(fieldDelim);
+    __sb.append(FieldFormatter.escapeAndEnclose(price==null?"null":"" + price, delimiters));
+    if (useRecordDelim) {
+      __sb.append(delimiters.getLinesTerminatedBy());
+    }
+    return __sb.toString();
+  }
+  public void toString0(DelimiterSet delimiters, StringBuilder __sb, char fieldDelim) {
+    __sb.append(FieldFormatter.escapeAndEnclose(id==null?"null":"" + id, delimiters));
+    __sb.append(fieldDelim);
+    __sb.append(FieldFormatter.escapeAndEnclose(name==null?"null":name, delimiters));
+    __sb.append(fieldDelim);
+    __sb.append(FieldFormatter.escapeAndEnclose(author==null?"null":author, delimiters));
+    __sb.append(fieldDelim);
+    __sb.append(FieldFormatter.escapeAndEnclose(subject==null?"null":subject, delimiters));
+    __sb.append(fieldDelim);
+    __sb.append(FieldFormatter.escapeAndEnclose(price==null?"null":"" + price, delimiters));
+  }
+  private static final DelimiterSet __inputDelimiters = new DelimiterSet((char) 44, (char) 10, (char) 0, (char) 0, false);
+  private RecordParser __parser;
+  public void parse(Text __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  public void parse(CharSequence __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  public void parse(byte [] __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  public void parse(char [] __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  public void parse(ByteBuffer __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  public void parse(CharBuffer __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  private void __loadFromFields(List<String> fields) {
+    Iterator<String> __it = fields.listIterator();
+    String __cur_str = null;
+    try {
+    if (__it.hasNext()) {
+        __cur_str = __it.next();
+    } else {
+        __cur_str = "null";
+    }
+    if (__cur_str.equals("null") || __cur_str.length() == 0) { this.id = null; } else {
+      this.id = Integer.valueOf(__cur_str);
+    }
+
+    if (__it.hasNext()) {
+        __cur_str = __it.next();
+    } else {
+        __cur_str = "null";
+    }
+    if (__cur_str.equals("null")) { this.name = null; } else {
+      this.name = __cur_str;
+    }
+
+    if (__it.hasNext()) {
+        __cur_str = __it.next();
+    } else {
+        __cur_str = "null";
+    }
+    if (__cur_str.equals("null")) { this.author = null; } else {
+      this.author = __cur_str;
+    }
+
+    if (__it.hasNext()) {
+        __cur_str = __it.next();
+    } else {
+        __cur_str = "null";
+    }
+    if (__cur_str.equals("null")) { this.subject = null; } else {
+      this.subject = __cur_str;
+    }
+
+    if (__it.hasNext()) {
+        __cur_str = __it.next();
+    } else {
+        __cur_str = "null";
+    }
+    if (__cur_str.equals("null") || __cur_str.length() == 0) { this.price = null; } else {
+      this.price = Float.valueOf(__cur_str);
+    }
+
+    } catch (RuntimeException e) {    throw new RuntimeException("Can't parse input data: '" + __cur_str + "'", e);    }  }
+
+  private void __loadFromFields0(Iterator<String> __it) {
+    String __cur_str = null;
+    try {
+    if (__it.hasNext()) {
+        __cur_str = __it.next();
+    } else {
+        __cur_str = "null";
+    }
+    if (__cur_str.equals("null") || __cur_str.length() == 0) { this.id = null; } else {
+      this.id = Integer.valueOf(__cur_str);
+    }
+
+    if (__it.hasNext()) {
+        __cur_str = __it.next();
+    } else {
+        __cur_str = "null";
+    }
+    if (__cur_str.equals("null")) { this.name = null; } else {
+      this.name = __cur_str;
+    }
+
+    if (__it.hasNext()) {
+        __cur_str = __it.next();
+    } else {
+        __cur_str = "null";
+    }
+    if (__cur_str.equals("null")) { this.author = null; } else {
+      this.author = __cur_str;
+    }
+
+    if (__it.hasNext()) {
+        __cur_str = __it.next();
+    } else {
+        __cur_str = "null";
+    }
+    if (__cur_str.equals("null")) { this.subject = null; } else {
+      this.subject = __cur_str;
+    }
+
+    if (__it.hasNext()) {
+        __cur_str = __it.next();
+    } else {
+        __cur_str = "null";
+    }
+    if (__cur_str.equals("null") || __cur_str.length() == 0) { this.price = null; } else {
+      this.price = Float.valueOf(__cur_str);
+    }
+
+    } catch (RuntimeException e) {    throw new RuntimeException("Can't parse input data: '" + __cur_str + "'", e);    }  }
+
+  public Object clone() throws CloneNotSupportedException {
+    books o = (books) super.clone();
+    return o;
+  }
+
+  public void clone0(books o) throws CloneNotSupportedException {
+  }
+
+  public Map<String, Object> getFieldMap() {
+    Map<String, Object> __sqoop$field_map = new HashMap<String, Object>();
+    __sqoop$field_map.put("id", this.id);
+    __sqoop$field_map.put("name", this.name);
+    __sqoop$field_map.put("author", this.author);
+    __sqoop$field_map.put("subject", this.subject);
+    __sqoop$field_map.put("price", this.price);
+    return __sqoop$field_map;
+  }
+
+  public void getFieldMap0(Map<String, Object> __sqoop$field_map) {
+    __sqoop$field_map.put("id", this.id);
+    __sqoop$field_map.put("name", this.name);
+    __sqoop$field_map.put("author", this.author);
+    __sqoop$field_map.put("subject", this.subject);
+    __sqoop$field_map.put("price", this.price);
+  }
+
+  public void setField(String __fieldName, Object __fieldVal) {
+    if (!setters.containsKey(__fieldName)) {
+      throw new RuntimeException("No such field:"+__fieldName);
+    }
+    setters.get(__fieldName).setField(__fieldVal);
+  }
+
+}
